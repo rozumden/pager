58297927d1f2266c01a628dc719615a4cf42e585
Kamil <sveckamil@gmail.com>
2016-05-18 13:19:18 +0200

    connect to server

--- server.c
+++ server.c
@@ -2,7 +2,27 @@
 
 int socket;
 
-int connect(int pager_id, char * address, int port = 55556) {
-		socket = socket(AF_INET, SOCK_STREAM, 0);
-		return 1;
+void connect(int pager_id, char * address, int port = 55556) {
+	socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket < 0) {
+		perror("Socket cannot be created");
+		return;
+	}
+	
+	struct sockaddr_in server;
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+	if (inet_pton(AF_INET, address, &server.sin_addr) != 1) {
+		close(socket);
+		perror("invalid address family or IP address");
+		return;
+	}
+	
+	if (connect(socket, (struct sockaddr *) &server, sizeof(server)) != 0) {
+		perror("Unable to connect to server.");
+	}
+	
+	return 1;
 }
+
